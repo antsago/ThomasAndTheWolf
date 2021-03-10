57e7c372767f8114b8573e50ad2c123f8a168f54
Antonio <sago.ant@gmail.com>
2021-03-10 20:05:49 +0100

    feat: load layout

--- LoadPuzzle.js
+++ LoadPuzzle.js
@@ -1,3 +1,18 @@
+function createColumnsOfRow(layout, rowNumber) {
+  return layout
+    .filter((cell) => cell.row === rowNumber)
+    .reduce((columns, cell) => {
+      if (cell.column in columns) {
+        return columns;
+      }
+
+      return {
+        ...columns,
+        [cell.column]: cell,
+      };
+    }, {});
+}
+
 function loadPuzzle(puzzle) {
   return {
     name: puzzle.name,
@@ -5,6 +20,26 @@ function loadPuzzle(puzzle) {
     wolf: puzzle.wolf,
     isThomasTurn: true,
     thomasState: "running",
+    layout: puzzle.layout.reduce((rows, rowCell) => {
+      if (rowCell.row in rows) {
+        return rows;
+      }
+
+      return {
+        ...rows,
+        [rowCell.row]: puzzle.layout
+          .filter((cell) => cell.row === rowCell.row)
+          .reduce((columns, columnCell) => {
+            if (columnCell.column in columnCell) {
+              return columns;
+            }
+            return {
+              ...columns,
+              [columnCell.column]: columnCell,
+            };
+          }, {}),
+      };
+    }, {}),
   };
 }
 
--- LoadPuzzle.test.js
+++ LoadPuzzle.test.js
@@ -4,9 +4,14 @@ describe("Load puzzle", () => {
   test("Returns basic information", () => {
     const puzzle = {
       name: "puzzle1",
-      layout: [],
-      wolf: { row: 3, column: 6 },
-      thomas: { row: 3, column: 4 },
+      wolf: { row: 2, column: 2 },
+      thomas: { row: 1, column: 1 },
+      layout: [
+        { row: 1, column: 1, borders: "T" },
+        { row: 1, column: 2, borders: "TR" },
+        { row: 2, column: 1, borders: "LB" },
+        { row: 2, column: 2, borders: "BR" },
+      ],
     };
 
     const state = loadPuzzle(puzzle);
@@ -17,6 +22,16 @@ describe("Load puzzle", () => {
       isThomasTurn: true,
       name: puzzle.name,
       thomasState: "running",
+      layout: {
+        1: {
+          1: puzzle.layout[0],
+          2: puzzle.layout[1],
+        },
+        2: {
+          1: puzzle.layout[2],
+          2: puzzle.layout[3],
+        },
+      },
     });
   });
 });
