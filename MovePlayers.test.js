e92aa6c31b468f08971526bd7b32a1c4a1030968
Antonio <sago.ant@gmail.com>
2021-03-11 08:56:13 +0100

    feat: new test case

--- MovePlayers.test.js
+++ MovePlayers.test.js
@@ -24,4 +24,27 @@ describe("Players moves", () => {
 
     expect(result.getPuzzle()).toEqual(expected.getPuzzle());
   });
+
+  test("Moving left changes Thomas position", () => {
+    const baseConfig = {
+      name: "testPuzzle",
+      thomas: { row: 2, column: 2 },
+      wolf: { row: 1, column: 1 },
+      layout: [
+        { row: 1, column: 1, borders: "T" },
+        { row: 1, column: 2, borders: "TR" },
+        { row: 2, column: 1, borders: "LB" },
+        { row: 2, column: 2, borders: "BR" },
+      ],
+    };
+    const initial = PuzzleFactory.fromConfig(baseConfig);
+    const expected = PuzzleFactory.fromConfig({
+      ...baseConfig,
+      thomas: { row: 2, column: 2 },
+    });
+
+    const result = MovePlayers("Thomas-Up", initial);
+
+    expect(result.getPuzzle()).toEqual(expected.getPuzzle());
+  });
 });
