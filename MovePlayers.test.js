6d00e3392d7a8fcd66721d9bdde965aca249a8db
Antonio <sago.ant@gmail.com>
2021-03-11 08:59:50 +0100

    refactor: extracted testPuzzle generator

--- MovePlayers.test.js
+++ MovePlayers.test.js
@@ -1,22 +1,31 @@
 import PuzzleFactory from "./PuzzleFactory";
 import MovePlayers from "./MovePlayers";
 
+function testPuzzle(initialConfig, expectedConfig) {
+  const baseConfig = {
+    name: "testPuzzle",
+    thomas: { row: 2, column: 2 },
+    wolf: { row: 1, column: 1 },
+    layout: [
+      { row: 1, column: 1, borders: "T" },
+      { row: 1, column: 2, borders: "TR" },
+      { row: 2, column: 1, borders: "LB" },
+      { row: 2, column: 2, borders: "BR" },
+    ],
+    ...initialConfig,
+  };
+  const initial = PuzzleFactory.fromConfig(baseConfig);
+  const expected = PuzzleFactory.fromConfig({
+    ...baseConfig,
+    ...expectedConfig,
+  });
+
+  return { initial, expected };
+}
+
 describe("Players moves", () => {
   test("Moving up changes Thomas position", () => {
-    const baseConfig = {
-      name: "testPuzzle",
-      thomas: { row: 2, column: 2 },
-      wolf: { row: 1, column: 1 },
-      layout: [
-        { row: 1, column: 1, borders: "T" },
-        { row: 1, column: 2, borders: "TR" },
-        { row: 2, column: 1, borders: "LB" },
-        { row: 2, column: 2, borders: "BR" },
-      ],
-    };
-    const initial = PuzzleFactory.fromConfig(baseConfig);
-    const expected = PuzzleFactory.fromConfig({
-      ...baseConfig,
+    const { initial, expected } = testPuzzle(undefined, {
       thomas: { row: 1, column: 2 },
     });
 
@@ -26,24 +35,11 @@ describe("Players moves", () => {
   });
 
   test("Moving left changes Thomas position", () => {
-    const baseConfig = {
-      name: "testPuzzle",
-      thomas: { row: 2, column: 2 },
-      wolf: { row: 1, column: 1 },
-      layout: [
-        { row: 1, column: 1, borders: "T" },
-        { row: 1, column: 2, borders: "TR" },
-        { row: 2, column: 1, borders: "LB" },
-        { row: 2, column: 2, borders: "BR" },
-      ],
-    };
-    const initial = PuzzleFactory.fromConfig(baseConfig);
-    const expected = PuzzleFactory.fromConfig({
-      ...baseConfig,
-      thomas: { row: 2, column: 2 },
+    const { initial, expected } = testPuzzle(undefined, {
+      thomas: { row: 2, column: 1 },
     });
 
-    const result = MovePlayers("Thomas-Up", initial);
+    const result = MovePlayers("Thomas-Left", initial);
 
     expect(result.getPuzzle()).toEqual(expected.getPuzzle());
   });
