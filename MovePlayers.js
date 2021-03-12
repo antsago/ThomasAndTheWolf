14009179b5bbef6616686076d1848312f8b9a2d9
Antonio <sago.ant@gmail.com>
2021-03-12 17:29:08 +0100

    feat: handled Thomas move left

--- MovePlayers.js
+++ MovePlayers.js
@@ -2,7 +2,10 @@ import PuzzleFactory from "./PuzzleFactory";
 
 function movePlayer(move, puzzle) {
   const currentPosition = puzzle.thomas;
-  const newPosition = { ...currentPosition, row: currentPosition.row - 1 };
+  const newPosition =
+    move === "Thomas-Up"
+      ? { ...currentPosition, row: currentPosition.row - 1 }
+      : { ...currentPosition, column: currentPosition.column - 1 };
 
   const updatedPuzzle = PuzzleFactory.fromPuzzle(puzzle)
     .addThomas(newPosition.row, newPosition.column)
