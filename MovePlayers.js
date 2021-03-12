db8849b39f10eacfd054cf1914c6d4480b31d0bf
Antonio <sago.ant@gmail.com>
2021-03-12 17:41:26 +0100

    feat: move thomas right

--- MovePlayers.js
+++ MovePlayers.js
@@ -1,11 +1,20 @@
 import PuzzleFactory from "./PuzzleFactory";
 
+function getNewPosition(move, currentPosition) {
+  switch (move) {
+    case "Thomas-Up":
+      return { ...currentPosition, row: currentPosition.row - 1 };
+    case "Thomas-Left":
+      return { ...currentPosition, column: currentPosition.column - 1 };
+    case "Thomas-Right":
+      return { ...currentPosition, column: currentPosition.column + 1 };
+    default:
+      throw new Error("Move not valid");
+  }
+}
+
 function movePlayer(move, puzzle) {
-  const currentPosition = puzzle.thomas;
-  const newPosition =
-    move === "Thomas-Up"
-      ? { ...currentPosition, row: currentPosition.row - 1 }
-      : { ...currentPosition, column: currentPosition.column - 1 };
+  const newPosition = getNewPosition(move, puzzle.thomas);
 
   const updatedPuzzle = PuzzleFactory.fromPuzzle(puzzle)
     .addThomas(newPosition.row, newPosition.column)
