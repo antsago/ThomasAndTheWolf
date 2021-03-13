38afdf2151ec89e6d1a501ce12ce97615e52732b
Antonio <sago.ant@gmail.com>
2021-03-13 13:42:06 +0100

    refactor: extracted directions logic

--- WolfMoveGenerator.js
+++ WolfMoveGenerator.js
@@ -1,21 +1,53 @@
 import { Borders, Moves, Players } from "./constants";
 
-function WolfMoveGenerator(puzzle) {
-  const thomasPosition = puzzle[Players.Thomas];
-  const wolfPosition = puzzle[Players.Wolf];
-
-  const wolfCell = puzzle.layout[wolfPosition.row][wolfPosition.column];
-
+function vertically(thomasPosition, wolfPosition, wolfCell) {
   if (thomasPosition.row < wolfPosition.row) {
     return Moves.Up;
   }
   if (thomasPosition.row > wolfPosition.row) {
     return wolfCell.borders.includes(Borders.Bottom) ? Moves.Stay : Moves.Down;
   }
+
+  // In Thomas' cell already
+  return Moves.Stay;
+}
+
+function horizontally(thomasPosition, wolfPosition, wolfCell) {
   if (thomasPosition.column < wolfPosition.column) {
     return Moves.Left;
   }
-  return Moves.Right;
+  if (thomasPosition.column > wolfPosition.column) {
+    return Moves.Right;
+  }
+
+  // In Thomas' cell already
+  return Moves.Stay;
+}
+
+function diagonally(thomasPosition, wolfPosition, wolfCell) {
+  if (thomasPosition.row < wolfPosition.row) {
+    return Moves.Up;
+  }
+  if (thomasPosition.row > wolfPosition.row) {
+    return wolfCell.borders.includes(Borders.Bottom) ? Moves.Stay : Moves.Down;
+  }
+
+  throw new Error("This should never happen");
+}
+
+function WolfMoveGenerator(puzzle) {
+  const thomasPosition = puzzle[Players.Thomas];
+  const wolfPosition = puzzle[Players.Wolf];
+
+  const wolfCell = puzzle.layout[wolfPosition.row][wolfPosition.column];
+
+  if (thomasPosition.column === wolfPosition.column) {
+    return vertically(thomasPosition, wolfPosition, wolfCell);
+  }
+  if (thomasPosition.row === wolfPosition.row) {
+    return horizontally(thomasPosition, wolfPosition, wolfCell);
+  }
+  return diagonally(thomasPosition, wolfPosition, wolfCell);
 }
 
 export default WolfMoveGenerator;
