893827809f652d6a15dc580fa3ac2dbb343d36dc
Antonio <sago.ant@gmail.com>
2021-03-13 20:34:13 +0100

    feat: added express

--- src/index.js
+++ src/index.js
@@ -1,1 +1,7 @@
-console.log("Hello world");
+import express from "express";
+
+const PORT = 3000 || process.env.PORT;
+
+express()
+  .get("/", (req, res) => res.send("Hello world"))
+  .listen(PORT, () => console.log(`Server listening at port ${PORT}`));
