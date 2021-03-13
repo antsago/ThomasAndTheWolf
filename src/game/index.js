d7fe59c185a9583ca12bfc6cd45ae5bd2a337853
Antonio <sago.ant@gmail.com>
2021-03-13 20:17:49 +0100

    refactor: created game submodule

---
+++ src/game/index.js
@@ -0,0 +1,3 @@
+export { default as PuzzleFactory } from "./PuzzleFactory";
+export { default as MoveBuilder } from "./MoveBuilder";
+export { default as WolfMoveGenerator } from "./WolfMoveGenerator";
