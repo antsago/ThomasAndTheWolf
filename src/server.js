e21b3252ff2d3c3516b6f0176b05cac6fd356c2a
Antonio <sago.ant@gmail.com>
2021-03-13 22:37:55 +0100

    refactor: extracted server components

--- src/server.js
+++ src/server.js
@@ -1,38 +1,39 @@
 import express from "express";
 import { MoveBuilder, PuzzleFactory } from "./game";
 
-const server = express()
-  .use(express.json())
-  .use(
-    "/game",
-    express
-      .Router()
-      .post("/load", (req, res) =>
-        res
-          .status(200)
-          .json(
-            PuzzleFactory.fromConfigArray(req.body).map((puzzle) =>
-              puzzle.getPuzzle()
-            )
-          )
-      )
-      .post("/move", (req, res) =>
-        res
-          .status(200)
-          .json(
-            MoveBuilder.move(
-              req.body.player,
-              req.body.move,
-              req.body.puzzle
-            ).getPuzzle()
-          )
+const gameApi = express
+  .Router()
+  .post("/load", (req, res) =>
+    res
+      .status(200)
+      .json(
+        PuzzleFactory.fromConfigArray(req.body).map((puzzle) =>
+          puzzle.getPuzzle()
+        )
       )
   )
-  // Number of arguments mark this function as error handling
-  // eslint-disable-next-line no-unused-vars
-  .use((error, req, res, next) => {
-    console.error(error); // eslint-disable-line no-console
-    res.status(500).send();
-  });
+  .post("/move", (req, res) =>
+    res
+      .status(200)
+      .json(
+        MoveBuilder.move(
+          req.body.player,
+          req.body.move,
+          req.body.puzzle
+        ).getPuzzle()
+      )
+  );
+
+// Number of arguments marks this function as error handling
+// eslint-disable-next-line no-unused-vars
+function errorHandling(error, req, res, next) {
+  console.error(error); // eslint-disable-line no-console
+  res.status(500).send();
+}
+
+const server = express()
+  .use(express.json())
+  .use("/game", gameApi)
+  .use(errorHandling);
 
 export default server;
